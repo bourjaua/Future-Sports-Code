4eaabc56c3d41dbc99fc2f385604124c839ee9bc
bourjaua <alex.bourjau@hotmail.com>
2023-09-20 15:15:51 +1000

    ready to deploy stage 2
    added relevant changes to the index and about with all pages being used in app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,14 +3,29 @@ import ReactDOM from 'react-dom/client';
 import "./styles.css"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from './Components/Pages/Home';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <HashRouter>
+  <Routes>
+  <Route path="/" element={<App/>} />
+  <Route path="/Home" element={<Home/>}/>
+  <Route path="/BecomeAMember" element={<BecomeAMember/>}/>
+  <Route path="/CommunityBlog" element={<CommunityBlog/>}/>
+  <Route path="/ContactUs" element={<ContactUs/>}/>
+  <Route path="/JuniorDivision" element={<JuniorDivision/>}/>
+  <Route path="/Login" element={<Login/>}/>
+  <Route path="/PastRaces" element={<PastRaces/>}/>
+  <Route path="/TrainingSchedule" element={<TrainingSchedule/>}/>
+  <Route path="/UpcomingRaces" element={<UpcomingRaces/>}/>
+
+  </Routes>
+  </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+ );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
